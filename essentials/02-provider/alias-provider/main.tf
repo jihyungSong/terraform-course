c7567684dd57f06a50ef10a2144e32867d613f16
Jihyungsong <bluese05@gmail.com>
2021-09-05 06:30:28 +0000

    update

--- essentials/02-provider/alias-provider/main.tf
+++ essentials/02-provider/alias-provider/main.tf
@@ -1,30 +1,30 @@
 terraform {}
 
 provider "aws" {
-    region = "us-east-1"
+    region          =   "us-east-1"
 }
 
 provider "aws" {
-    alias  = "west"
-    region = "us-west-2"
+    alias           =   "west"
+    region          =   "us-west-2"
 }
 
 
 resource "aws_vpc" "east_main" {
-    cidr_block = "10.0.0.0/16"
+    cidr_block      =   "10.0.0.0/16"
 
     tags = {
-        Name = "ssuser01-vpc-east-1"
-        Managed_by = "terraform"
+        Name        =   "<<VPC_NAME>>"
+        Managed_by  =   "terraform"
     }
 }
 
 resource "aws_vpc" "west_main" {
-    provider   = aws.west
-    cidr_block = "10.0.0.0/16"
+    provider        =   aws.west
+    cidr_block      =   "10.0.0.0/16"
 
     tags = {
-        Name = "ssuser01-vpc-west-2"
-        Managed_by = "terraform"
+        Name        =   "<<VPC_NAME>>"
+        Managed_by  =   "terraform"
     }
 }
