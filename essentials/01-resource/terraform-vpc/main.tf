9d3f2b99eae7e905fe0bc54f3bdac6be05eb7ab1
Jihyungsong <bluese05@gmail.com>
2021-09-05 05:08:03 +0000

    update

--- essentials/01-resource/terraform-vpc/main.tf
+++ essentials/01-resource/terraform-vpc/main.tf
@@ -1,14 +1,14 @@
 terraform {}
 
 provider "aws" {
-    region = "us-east-1"
+    region          =   "<<REGION_NAME>>"
 }
 
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
+    cidr_block      =   "10.0.0.0/16"
 
     tags = {
-        Name = "ssuser01-vpc"
-        Managed_by = "terraform"
+        Name        =   "<<VPC_NAME>>"
+        Managed_by  =   "terraform"
     }
 }
