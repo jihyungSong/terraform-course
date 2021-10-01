21dd665edb2d2001122ba736f882853a3b7857a2
EC2 Default User <ec2-user@ip-172-31-29-15.eu-west-1.compute.internal>
2021-10-01 07:46:26 +0000

    typo

--- aws/12-alb/modules/vpc/route_table.tf
+++ aws/12-alb/modules/vpc/route_table.tf
@@ -35,7 +35,7 @@ resource "aws_route_table_association" "public" {
 }
 
 resource "aws_route_table_association" "private" {
-    count               =   length(var.public_subnets)
+    count               =   length(var.private_subnets)
     
     subnet_id           =   "${aws_subnet.private[count.index].id}"
     route_table_id      =   aws_route_table.private.id
